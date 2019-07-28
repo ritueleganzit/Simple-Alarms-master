ed18fcd33b65a1b5aa4e22cc7f5dc0ac732721f3
riturajan <ritu.eleganzit@gmail.com>
2019-07-28 23:17:46 +0530

    Initial commit

---
+++ app/src/main/java/com/github/ppartisan/simplealarms/ui/AddEditAlarmFragment.java
@@ -0,0 +1,185 @@
+package com.github.ppartisan.simplealarms.ui;
+
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AlertDialog;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.TimePicker;
+import android.widget.Toast;
+
+import com.github.ppartisan.simplealarms.R;
+import com.github.ppartisan.simplealarms.data.DatabaseHelper;
+import com.github.ppartisan.simplealarms.model.Alarm;
+import com.github.ppartisan.simplealarms.service.AlarmReceiver;
+import com.github.ppartisan.simplealarms.service.LoadAlarmsService;
+import com.github.ppartisan.simplealarms.util.ViewUtils;
+
+import java.util.Calendar;
+
+public final class AddEditAlarmFragment extends Fragment {
+
+    private TimePicker mTimePicker;
+    private EditText mLabel;
+    int hour[]={8,9,10,11,12,13,14,15,16,17,18,19,20,21};
+    private CheckBox mMon, mTues, mWed, mThurs, mFri, mSat, mSun;
+
+    public static AddEditAlarmFragment newInstance(Alarm alarm) {
+
+        Bundle args = new Bundle();
+        args.putParcelable(AddEditAlarmActivity.ALARM_EXTRA, alarm);
+
+        AddEditAlarmFragment fragment = new AddEditAlarmFragment();
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+        final View v = inflater.inflate(R.layout.fragment_add_edit_alarm, container, false);
+
+        setHasOptionsMenu(true);
+
+        final Alarm alarm = getAlarm();
+
+        mTimePicker = (TimePicker) v.findViewById(R.id.edit_alarm_time_picker);
+        ViewUtils.setTimePickerTime(mTimePicker, alarm.getTime());
+
+        mLabel = (EditText) v.findViewById(R.id.edit_alarm_label);
+        mLabel.setText(alarm.getLabel());
+
+        mMon = (CheckBox) v.findViewById(R.id.edit_alarm_mon);
+        mTues = (CheckBox) v.findViewById(R.id.edit_alarm_tues);
+        mWed = (CheckBox) v.findViewById(R.id.edit_alarm_wed);
+        mThurs = (CheckBox) v.findViewById(R.id.edit_alarm_thurs);
+        mFri = (CheckBox) v.findViewById(R.id.edit_alarm_fri);
+        mSat = (CheckBox) v.findViewById(R.id.edit_alarm_sat);
+        mSun = (CheckBox) v.findViewById(R.id.edit_alarm_sun);
+
+        setDayCheckboxes(alarm);
+
+        return v;
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        inflater.inflate(R.menu.edit_alarm_menu, menu);
+        super.onCreateOptionsMenu(menu, inflater);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_save:
+                save();
+                break;
+            case R.id.action_delete:
+                delete();
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private Alarm getAlarm() {
+        return getArguments().getParcelable(AddEditAlarmActivity.ALARM_EXTRA);
+    }
+
+    private void setDayCheckboxes(Alarm alarm) {
+        mMon.setChecked(alarm.getDay(Alarm.MON));
+        mTues.setChecked(alarm.getDay(Alarm.TUES));
+        mWed.setChecked(alarm.getDay(Alarm.WED));
+        mThurs.setChecked(alarm.getDay(Alarm.THURS));
+        mFri.setChecked(alarm.getDay(Alarm.FRI));
+        mSat.setChecked(alarm.getDay(Alarm.SAT));
+        mSun.setChecked(alarm.getDay(Alarm.SUN));
+    }
+
+    private void save() {
+
+
+
+        for (int i=0;i<hour.length;i++)
+        {
+
+
+            final long id = DatabaseHelper.getInstance(getActivity()).addAlarm();
+            LoadAlarmsService.launchLoadAlarmsService(getActivity());
+            Alarm alarm = new Alarm(id);
+            Log.d("dateeee",""+hour[i]);
+            final Calendar time = Calendar.getInstance();
+            time.set(Calendar.MINUTE, 35);
+            time.set(Calendar.HOUR_OF_DAY, hour[i]);
+            alarm.setTime(time.getTimeInMillis());
+
+            Log.d("dateeee",""+time.getTimeInMillis());
+
+
+            alarm.setLabel(""+hour[i]);
+
+            alarm.setDay(Alarm.MON, true);
+            alarm.setDay(Alarm.TUES,true);
+            alarm.setDay(Alarm.WED, true);
+            alarm.setDay(Alarm.THURS, true);
+            alarm.setDay(Alarm.FRI, true);
+            alarm.setDay(Alarm.SAT, true);
+            alarm.setDay(Alarm.SUN, false);
+
+            final int rowsUpdated = DatabaseHelper.getInstance(getContext()).updateAlarm(alarm);
+            final int messageId = (rowsUpdated == 1) ? R.string.update_complete : R.string.update_failed;
+
+            Toast.makeText(getContext(), messageId, Toast.LENGTH_SHORT).show();
+
+            AlarmReceiver.setReminderAlarm(getContext(), alarm);
+
+
+
+        }
+
+        getActivity().finish();
+    }
+
+    private void delete() {
+
+        final Alarm alarm = getAlarm();
+
+        final AlertDialog.Builder builder =
+                new AlertDialog.Builder(getContext(), R.style.DeleteAlarmDialogTheme);
+        builder.setTitle(R.string.delete_dialog_title);
+        builder.setMessage(R.string.delete_dialog_content);
+        builder.setPositiveButton(R.string.yes, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+
+                //Cancel any pending notifications for this alarm
+                AlarmReceiver.cancelReminderAlarm(getContext(), alarm);
+
+                final int rowsDeleted = DatabaseHelper.getInstance(getContext()).deleteAlarm(alarm);
+                int messageId;
+                if(rowsDeleted == 1) {
+                    messageId = R.string.delete_complete;
+                    Toast.makeText(getContext(), messageId, Toast.LENGTH_SHORT).show();
+                    LoadAlarmsService.launchLoadAlarmsService(getContext());
+                    getActivity().finish();
+                } else {
+                    messageId = R.string.delete_failed;
+                    Toast.makeText(getContext(), messageId, Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+        builder.setNegativeButton(R.string.no, null);
+        builder.show();
+
+    }
+
+}
